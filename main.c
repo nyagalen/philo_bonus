96ce35c92df4b9743069d8eca2fe426ffd8cba9d
svetlana <svydrina@LAPTOP-57CP0962>
2023-12-30 08:27:11 +0100

    leaks fixed

--- main.c
+++ main.c
@@ -6,11 +6,23 @@
 /*   By: svydrina <svydrina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 13:01:54 by svydrina          #+#    #+#             */
-/*   Updated: 2023/12/30 07:25:51 by svydrina         ###   ########.fr       */
+/*   Updated: 2023/12/30 08:25:27 by svydrina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	free_all(t_philo *philos, t_data *data)
+{
+	int	i;
+	
+	i = -1;
+	while (++i < data->nb_ph)
+		sem_close(philos[i].meal_s);
+	sem_close(data->forks);
+	sem_close(data->death_s);
+	sem_close(data->print_s);
+}
 
 int	main(int argc, char **argv)
 {
@@ -28,5 +40,6 @@ int	main(int argc, char **argv)
 		printf("Malloc error\n");
 		return (1);
 	}
+	free_all(philos, &data);
 	return (0);
 }
