de6d96f2430b1a873544c1c767a1a2f2f8327955
svetlana <svydrina@LAPTOP-57CP0962>
2023-12-30 05:19:20 +0100

    sss

--- init.c
+++ init.c
@@ -6,11 +6,35 @@
 /*   By: svydrina <svydrina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 12:30:04 by svydrina          #+#    #+#             */
-/*   Updated: 2023/12/30 04:11:19 by svydrina         ###   ########.fr       */
+/*   Updated: 2023/12/30 04:43:00 by svydrina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	init_philos(t_philo *philos, t_data *data)
+{
+	int		i;
+	char	*sem_name;
+	char	*id_str;
+
+	i = -1;
+	while (++i < data->nb_ph)
+	{
+		philos[i].id = i + 1;
+		philos[i].meals = 0;
+		philos[i].last_meal = 0;
+		id_str = ft_itoa(philos[i].id);
+		sem_name = ft_strjoin("/meal_s", id_str);
+		if (!id_str || !sem_name)
+			return (0);
+		sem_unlink(sem_name);
+		philos[i].meal_s = sem_open(sem_name, O_CREAT, 0644, 1);
+		free(id_str);
+		free(sem_name);
+	}
+	return (1);
+}
 
 int	init_data(t_data *data, int argc, char **argv)
 {
